f26c54cb2f97f1f97748575a269c713162009af3
adrian <rq880113@163.com>
2017-01-09 18:33:41 +0800

    start opengl es

---
+++ app/src/main/java/com/adrian/opengles/FirstOpenGLRenderer.java
@@ -0,0 +1,29 @@
+package com.adrian.opengles;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.glClearColor;
+
+/**
+ * Created by adrian on 17-1-9.
+ */
+
+public class FirstOpenGLRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+}
--- app/src/main/java/com/adrian/opengles/MainActivity.java
+++ app/src/main/java/com/adrian/opengles/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +21,32 @@ public class MainActivity extends AppCompatActivity {
         ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         ConfigurationInfo configurationInfo = am.getDeviceConfigurationInfo();
         boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if (supportsEs2) {
+            glSurfaceView.setEGLContextClientVersion(2);
+
+            glSurfaceView.setRenderer(null);
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (rendererSet) {
+            glSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (rendererSet) {
+            glSurfaceView.onPause();
+        }
     }
 }
