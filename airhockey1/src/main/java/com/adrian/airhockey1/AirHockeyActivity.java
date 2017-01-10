6ad43c935a1c3a44394df1c04129fc5b8a29f448
ranqing <rq880113@163.com>
2017-01-11 02:53:11 +0800

    clear color

---
+++ airhockey1/src/main/java/com/adrian/airhockey1/AirHockeyActivity.java
@@ -0,0 +1,12 @@
+package com.adrian.airhockey1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AirHockeyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
