dfec4198554cab2321e4455703c2d19420fe679a
adrian <rq880113@163.com>
2017-01-12 18:41:17 +0800

    start opengl es

---
+++ airhockey1/src/main/java/com/adrian/airhockey1/AirHockeyRender.java
@@ -0,0 +1,32 @@
+package com.adrian.airhockey1;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+
+/**
+ * Created by adrian on 17-1-12.
+ */
+
+public class AirHockeyRender implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        glClearColor(1.0f, 0.0f, 0.0f, .0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
