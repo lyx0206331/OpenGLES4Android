e503bb41b75fe5a1ce0dba7d523626f8d76611db
ranqing <rq880113@163.com>
2017-01-13 00:45:27 +0800

    clear color

--- airhockey1/src/main/java/com/adrian/airhockey1/AirHockeyActivity.java
+++ airhockey1/src/main/java/com/adrian/airhockey1/AirHockeyActivity.java
@@ -1,12 +1,49 @@
 package com.adrian.airhockey1;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class AirHockeyActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        glSurfaceView = new GLSurfaceView(this);
+
+        ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo configurationInfo = am.getDeviceConfigurationInfo();
+        boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if (supportsEs2) {
+            glSurfaceView.setEGLContextClientVersion(2);
+
+            glSurfaceView.setRenderer(new AirHockeyRender());
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (rendererSet) glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (rendererSet) glSurfaceView.onPause();
     }
 }
--- airhockey1/src/main/java/com/adrian/airhockey1/AirHockeyRender.java
+++ airhockey1/src/main/java/com/adrian/airhockey1/AirHockeyRender.java
@@ -2,6 +2,10 @@ package com.adrian.airhockey1;
 
 import android.opengl.GLSurfaceView;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -15,9 +19,41 @@ import static android.opengl.GLES20.glViewport;
  */
 
 public class AirHockeyRender implements GLSurfaceView.Renderer {
+
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int BYTES_PER_FLOAT = 4;
+    private FloatBuffer vertexData;
+
+    public AirHockeyRender() {
+        float[] tableVertices = {
+                //Triangle1
+                0f, 0f,
+                9f, 14f,
+                0f, 14f,
+
+                //Triangle2
+                0f, 0f,
+                9f, 0f,
+                9f, 14f
+        };
+        float[] lingMallets = {
+                //center line
+                0f, 7f,
+                9f, 7f,
+
+                //Mallets
+                4.5f, 2f,
+                4.5f, 12f
+        };
+
+        vertexData = ByteBuffer.allocateDirect(tableVertices.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder()).asFloatBuffer();
+        vertexData.put(tableVertices);
+    }
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        glClearColor(1.0f, 0.0f, 0.0f, .0f);
+        glClearColor(1.0f, 0.0f, 1.0f, .0f);
     }
 
     @Override
