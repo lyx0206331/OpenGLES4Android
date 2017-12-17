81acd59fea150982a18f4fb7a0a1d0ce3e4586f4
ranqing <rq880113@163.com>
2017-12-17 23:44:56 +0800

    update gradle

--- app/src/main/java/com/adrian/opengles/MainActivity.java
+++ app/src/main/java/com/adrian/opengles/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -20,8 +21,13 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         ConfigurationInfo configurationInfo = am.getDeviceConfigurationInfo();
-        boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
-
+        boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000
+                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                && (Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")));
         if (supportsEs2) {
             glSurfaceView.setEGLContextClientVersion(2);
 
