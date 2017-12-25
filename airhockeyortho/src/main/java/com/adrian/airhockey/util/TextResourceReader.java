1280ecab19c521e182aa0b4dc2ca435306fbfa63
lyx0206331 <rq880113@163.com>
2017-12-25 18:35:41 +0800

    add airhockeyortho module

---
+++ airhockeyortho/src/main/java/com/adrian/airhockey/util/TextResourceReader.java
@@ -0,0 +1,35 @@
+package com.adrian.airhockey.util;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by adrian on 17-1-13.
+ */
+
+public class TextResourceReader {
+
+    public static String readTextFileFromResource(Context context, int resourceId) {
+        StringBuilder body = new StringBuilder();
+        try {
+            InputStream inputStream = context.getResources().openRawResource(resourceId);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String nextLine;
+            while ((nextLine = bufferedReader.readLine()) != null) {
+                body.append(nextLine);
+                body.append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Resources.NotFoundException e) {
+            throw new RuntimeException("Resource not found: " + resourceId, e);
+        }
+        return body.toString();
+    }
+}
