6dcf0cde6ccfd77767fd6c42a5a77d654c6ce9ac
lyx0206331 <rq880113@163.com>
2018-01-05 11:51:02 +0800

    update texture

---
+++ airhockeytextured/src/main/java/com/adrian/airhockeytextured/Constants.java
@@ -0,0 +1,9 @@
+package com.adrian.airhockeytextured;
+
+/**
+ * Created by qing on 2018/1/4 0004.
+ */
+
+public class Constants {
+    public static final int BYTES_PER_FLOAT = 4;
+}
---
+++ airhockeytextured/src/main/java/com/adrian/airhockeytextured/objects/Table.java
@@ -0,0 +1,8 @@
+package com.adrian.airhockeytextured.objects;
+
+/**
+ * Created by qing on 2018/1/4 0004.
+ */
+
+public class Table {
+}
