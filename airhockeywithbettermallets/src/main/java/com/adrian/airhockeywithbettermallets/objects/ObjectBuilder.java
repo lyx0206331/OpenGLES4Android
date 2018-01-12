7e4d94de11e9ce590a68868b82be8cb481386c38
lyx0206331 <rq880113@163.com>
2018-01-12 19:29:36 +0800

    add better mallets

--- airhockeywithbettermallets/src/main/java/com/adrian/airhockeywithbettermallets/objects/ObjectBuilder.java
+++ airhockeywithbettermallets/src/main/java/com/adrian/airhockeywithbettermallets/objects/ObjectBuilder.java
@@ -1,6 +1,15 @@
 package com.adrian.airhockeywithbettermallets.objects;
 
+import android.util.FloatMath;
+
+import com.adrian.airhockeywithbettermallets.util.Geometry;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static android.opengl.GLES10.GL_TRIANGLE_FAN;
+import static android.opengl.GLES20.GL_TRIANGLE_STRIP;
+import static android.opengl.GLES20.glDrawArrays;
 
 /**
  * 物体构建器
@@ -21,14 +30,11 @@ public class ObjectBuilder {
             this.vertexData = vertexData;
             this.drawList = drawList;
         }
-
-        private GeneratedData build() {
-            return new GeneratedData(vertexData, drawList);
-        }
     }
 
     private static final int FLOATS_PER_VERTEX = 3;
     private final float[] vertexData;
+    private final List<DrawCommand> drawList = new ArrayList<>();
     private int offset = 0;
 
     private ObjectBuilder(int sizeInVertices) {
@@ -55,5 +61,82 @@ public class ObjectBuilder {
      */
     private static int sizeOfOpenCylinderInVertices(int numPoints) {
         return (numPoints + 1) * 2;
+    }
+
+    private void appendCircle(Geometry.Circle circle, final int numPoints) {
+        final int startVertex = offset / FLOATS_PER_VERTEX;
+        final int numVertices = sizeOfCircleInVertices(numPoints);
+
+        //Center point of fan
+        vertexData[offset++] = circle.center.x;
+        vertexData[offset++] = circle.center.y;
+        vertexData[offset++] = circle.center.z;
+
+        //Fan around center point. <= is used because we want to generate the point at the starting angle twice to complete the fan.
+        for (int i = 0; i <= numPoints; i++) {
+            float angleInRadians = ((float) i / (float) numPoints) * ((float) Math.PI * 2f);
+
+            vertexData[offset++] = (float) (circle.center.x + circle.radius * Math.cos(angleInRadians));
+            vertexData[offset++] = circle.center.y;
+            vertexData[offset++] = (float) (circle.center.z + circle.radius * Math.sin(angleInRadians));
+        }
+
+        drawList.add(new DrawCommand() {
+            @Override
+            public void draw() {
+                glDrawArrays(GL_TRIANGLE_FAN, startVertex, numVertices);
+            }
+        });
+    }
+
+    private void appendOpenCylinder(Geometry.Cylinder cylinder, int numPoints) {
+        final int startVertex = offset / FLOATS_PER_VERTEX;
+        final int numVertices = sizeOfOpenCylinderInVertices(numPoints);
+        final float yStart = cylinder.center.y - (cylinder.height / 2f);
+        final float yEnd = cylinder.center.y + (cylinder.height / 2f);
+
+        for (int i = 0; i <= numPoints; i++) {
+            float angleInRadians = ((float) i / (float) numPoints) * ((float) Math.PI * 2f);
+
+            float xPosition = (float) (cylinder.center.x + cylinder.radius * Math.cos(angleInRadians));
+            float zPosition = (float) (cylinder.center.z + cylinder.radius * Math.sin(angleInRadians));
+
+            vertexData[offset++] = xPosition;
+            vertexData[offset++] = yStart;
+            vertexData[offset++] = zPosition;
+
+            vertexData[offset++] = xPosition;
+            vertexData[offset++] = yEnd;
+            vertexData[offset++] = zPosition;
+        }
+
+        drawList.add(new DrawCommand() {
+            @Override
+            public void draw() {
+                glDrawArrays(GL_TRIANGLE_STRIP, startVertex, numVertices);
+            }
+        });
+    }
+
+    private GeneratedData build() {
+        return new GeneratedData(vertexData, drawList);
+    }
+
+    /**
+     * 生成一个冰球
+     *
+     * @param puck
+     * @param numPoints
+     * @return
+     */
+    static GeneratedData createPuck(Geometry.Cylinder puck, int numPoints) {
+        int size = sizeOfCircleInVertices(numPoints) + sizeOfOpenCylinderInVertices(numPoints);
+        ObjectBuilder builder = new ObjectBuilder(size);
+
+        Geometry.Circle puckTop = new Geometry.Circle(puck.center.translateY(puck.height / 2f), puck.radius);
+        builder.appendCircle(puckTop, numPoints);
+        builder.appendOpenCylinder(puck, numPoints);
+
+        return builder.build();
     }
 }
