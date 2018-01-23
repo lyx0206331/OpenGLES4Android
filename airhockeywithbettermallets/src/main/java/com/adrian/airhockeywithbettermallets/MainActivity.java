926b15a8db76fb6303c7acb56f1fe486b81b16e3
lyx0206331 <rq880113@163.com>
2018-01-23 18:25:40 +0800

    add better mallets

--- airhockeywithbettermallets/src/main/java/com/adrian/airhockeywithbettermallets/MainActivity.java
+++ airhockeywithbettermallets/src/main/java/com/adrian/airhockeywithbettermallets/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 
@@ -22,15 +24,29 @@ public class MainActivity extends AppCompatActivity {
         ConfigurationInfo configurationInfo = am.getDeviceConfigurationInfo();
         boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
 
+        final AirHockeyRender airHockeyRender = new AirHockeyRender(this);
+
         if (supportsEs2) {
             glSurfaceView.setEGLContextClientVersion(2);
 
-            glSurfaceView.setRenderer(new AirHockeyRender(this));
+            glSurfaceView.setRenderer(airHockeyRender);
             rendererSet = true;
         } else {
             Toast.makeText(this, "This device does not support OpenGL ES 2.0", Toast.LENGTH_SHORT).show();
             return;
         }
+
+        glSurfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event != null) {
+                    //Convert touch coordinates into normalized device coordinates, keeping in mind that Android's Y coordinates are inverted.
+                    final float normalizedX = (event.getX() / (float) v.getWidth()) * 2 - 1;
+                    final float normalizedY = -((event.getY() / (float) v.getHeight()) * 2 - 1);
+                }
+                return false;
+            }
+        });
 
         setContentView(glSurfaceView);
     }
